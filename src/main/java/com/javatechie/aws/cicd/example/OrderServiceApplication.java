9936bb0a887775c3e5fdc154b6587a70060ddb07
mkappati309 <52052642+mkappati309@users.noreply.github.com>
2020-10-20 11:15:43 -0500

    added getMessage method.

--- src/main/java/com/javatechie/aws/cicd/example/OrderServiceApplication.java
+++ src/main/java/com/javatechie/aws/cicd/example/OrderServiceApplication.java
@@ -1,15 +1,15 @@
 package com.javatechie.aws.cicd.example;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Comparator;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @SpringBootApplication
 @RestController
@@ -28,5 +28,9 @@ public class OrderServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(OrderServiceApplication.class, args);
     }
-
+    
+    @GetMapping("/hello")
+    public String getMessage()	{
+    	return "Hello";
+    }
 }
